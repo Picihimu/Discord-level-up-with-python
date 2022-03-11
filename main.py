67cdc8c6939ffe4a872e51bd08c4b46d09045510
PicciHimu <90996064+Picihimu@users.noreply.github.com>
2022-03-11 16:07:01 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,42 @@
-import pyautogui #pip install PyAutoGUI
+
+import pyautogui  # pip install PyAutoGUI
 import time
+import random
+
+run = 0
+# def Convert(string):
+#         list1 = []
+#         list1[:0] = string
+#         return list1
+
+str1 ="Tohelpmakthiseasierwehavereatedprintablecardsothatyoucanwriteyourownkindnessquote tosend to a friend relative or even co-worker. Check out our featured Sunny Sentiments bouquet browse our collection of"
+# split = Convert(str1)
+bday = "happy birthday Nomaan Mia" #<@!760875948290736188>
+# print(split)
+
 a = 0
-while a <= 1000:
-    time.sleep(3)
-    pyautogui.typewrite('hello')
+while a <= 10000:
+    # text = random.choice(str1)
+    response_delay = random.randint(3, 7)
+    time.sleep(response_delay)
+
+    pyautogui.typewrite(bday)
+    time.sleep(response_delay)
     pyautogui.press('enter')
     a += 1
 
-    
-    
-    
-    
-    
-    
-    
-    import pyautogui #pip install PyAutoGUI
-import time
-
-run = 0
-while run <= 5:
-    time.sleep(2)
-    def Convert(string):
-        list1=[]
-        list1[:0]=string
-        return list1
-
-    str1="ABCD"
-    split = Convert(str1)
 
 
+    # range = 0
+    # # run_2 = 0
+    # # while run_2 <= 10:
+    # for i in split:
+    #     range += 1
+    #     pyautogui.typewrite(split[range])
+    #     time.sleep(2)
+    #     # pyautogui.press('enter')
+    # print(split[range])
+    # run += 1
+    #
 
-
-
-    range = 0
-    # run_2 = 0
-    # while run_2 <= 10:
-    for i in split:
-        pyautogui.typewrite(split[range])
-        time.sleep(2)
-        pyautogui.press('enter')
-    print(split)
-    run += 1
-    range += 1
     # run_2 += 1
